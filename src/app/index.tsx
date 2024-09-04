44cabe7476024c48e65568203ea6f2c58ad5d4f7
Eduardo Lima Costa <dev.eduardo.lc@gmail.com>
2024-09-04 11:47:59 -0300

    skeleton animation

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,15 @@
-import { theme } from "@/theme";
 import { useEffect } from "react";
 import { StyleSheet, useWindowDimensions, View } from "react-native";
 import Animated, { useAnimatedStyle, useSharedValue, withSequence, withTiming } from "react-native-reanimated";
+import { Skeleton } from "moti/skeleton";
+
+import { theme } from "@/theme";
+
+const skeletonColors = [
+  theme.colors.gray[600],
+  theme.colors.gray[700],
+  theme.colors.gray[600],
+];
 
 export default function Splash() {
   const logoScale = useSharedValue(1);
@@ -31,6 +39,21 @@ export default function Splash() {
     );
   };
 
+  const boxes = (column: "right" | "left") => {
+    const rest = column === "left" ? 0 : 1;
+
+    return Array.from({ length: 20 })
+      .filter((_, index) => index % 2 === rest)
+      .map((_, index) => {
+        const height = index % 2 === (column === "left" ? 0 : 1) ? 200 : 300; 
+        return (
+          <Animated.View key={index} style={[styles.box, { height }]}>
+            <Skeleton colors={skeletonColors} width="100%" height={height} />
+          </Animated.View>
+        );
+      });
+  }
+
   useEffect(() => {
     logoAnimation();
   }, []);
@@ -38,6 +61,14 @@ export default function Splash() {
   return (
     <View style={styles.container}>
       <Animated.Image source={require("@/assets/images/logo.png")} style={[styles.logo, logoAnimatedStyles]} />
+      <View style={styles.boxes}>
+        <View style={styles.column}>
+          {boxes("left")}
+        </View>
+        <View style={styles.column}>
+          {boxes("right")}
+        </View>
+      </View>
     </View>
   );
 };
@@ -52,5 +83,20 @@ const styles = StyleSheet.create({
   logo: {
     height: 64,
     width: 64,
-  }
+  },
+  boxes: {
+    flex: 1,
+    width: "100%",
+    flexDirection: "row",  
+    gap: 16,
+  },
+  box: {
+    width: "100%",
+    backgroundColor: theme.colors.gray[600],
+    borderRadius: 16,
+  },
+  column: {
+    flex: 1,
+    gap: 16,
+  },
 })
